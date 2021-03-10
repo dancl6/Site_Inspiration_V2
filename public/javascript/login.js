5104dd93e30bdf8fb44d33f36fa18d6b6741f289
Dan Lyons <danielclyons@yahoo.com>
2021-03-09 21:52:50 -0500

    removed rerouting of login to question1 page

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -20,7 +20,7 @@ async function loginFormHandler(event) {
         });
     
         if (response.ok) {
-          document.location.replace('/question1');
+          document.location.replace('/');
         } else {
           alert(response.statusText);
         }
