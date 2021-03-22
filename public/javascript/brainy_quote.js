c4c85ac6029d0f7337ad95e6193a620cf5a8e15d
Dan Lyons <danielclyons@yahoo.com>
2021-03-21 20:34:56 -0400

    implemented get for brainy quotes api

--- public/javascript/brainy_quote.js
+++ public/javascript/brainy_quote.js
@@ -5,26 +5,29 @@ async function loginFormHandler(event) {
     const category = document.querySelector('#quote-category').value.trim();
     // const password = document.querySelector('#password-login').value.trim();
   
-    if (category) {
-        const response = await fetch(`brainyquotes.vercel.app/quotes?${category}`, {
-          method: 'post',
-          body: JSON.stringify({
-            // username,
-            // password
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        });
+    // if (category) {
+        await fetch(`https://brainyquotes.vercel.app/quotes?category=${category}`, {
+          method: 'GET',
+        //   body: JSON.stringify({
+        //     // username,
+        //     // password
+        //   }),
+        //   headers: {
+        //     'Content-Type': 'application/json'
+        //   }
+        }).then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+        })
     
-        if (response.ok) {
+        // if (response.ok) {
         //   document.location.replace('/');
-            console.log("response is :", response)
+            // console.log("response is :", response)
 
-        } else {
-          alert(response.statusText);
-        }
-      }
+        // } else {
+        //   alert(response.statusText);
+        // }
+    //   }
   }
 // }
   document.querySelector('#submitButton').addEventListener('click', loginFormHandler);
